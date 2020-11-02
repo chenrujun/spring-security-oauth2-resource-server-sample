795591a524e42c20fd1ed3cbe91b923982f75492
Rujun Chen <rujche@microsoft.com>
2020-11-02 17:14:46 +0800

    Init project by https://start.spring.io/

---
+++ src/main/java/chenrujun/springsecurityoauth2resourceserversample/SpringSecurityOauth2ResourceServerSampleApplication.java
@@ -0,0 +1,13 @@
+package chenrujun.springsecurityoauth2resourceserversample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2ResourceServerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauth2ResourceServerSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chenrujun/springsecurityoauth2resourceserversample/SpringSecurityOauth2ResourceServerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package chenrujun.springsecurityoauth2resourceserversample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2ResourceServerSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
